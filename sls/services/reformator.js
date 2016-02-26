a0ad46cc9e63be034189d36cede8dce9b952cdea
akysil <alex.kysil@gmail.com>
2016-02-26 17:22:27 +0200

    reformator refact

--- sls/services/reformator.js
+++ sls/services/reformator.js
@@ -1,3 +1,4 @@
+
 (function() {
     'use strict';
 
@@ -6,7 +7,7 @@
         .factory('Reformator', Reformator);
 
     Reformator.$inject = [];
-    
+
     function Reformator() {
 
         var service = {
@@ -16,28 +17,28 @@
         return service;
 
         function menu(data) {
-            
+ 
             var productsForMenu = [];
 
             for (var i = 0, j = data.length; i < j; i++) {
-                productsForMenu = productsForMenu.concat(composeNewProducts(data[i]));
+                productsForMenu = productsForMenu.concat(productsFromProduct(data[i]));
             }
 
             return productsForMenu;
 
-            function composeNewProducts(product) {
-                
+            function productsFromProduct(product) {
+
                 var newProducts = [];
-                
+
                 for (var i = 0, j = product.category.length; i < j; i++) {
 
                     newProducts[newProducts.length] = {
-                        title: product.title,
-                        link: product.link,
-                        categoryTitle: product.category[i].title,
-                        categoryLink: product.category[i].link,
+                        title:            product.title,
+                        link:             product.link,
+                        categoryTitle:    product.category[i].title,
+                        categoryLink:     product.category[i].link,
                         categoryPriority: product.category[i].priority,
-                        categorySide: product.category[i].side
+                        categorySide:     product.category[i].side
                     };
 
                 }
@@ -49,5 +50,5 @@
         }
 
     }
-        
+
 })();
